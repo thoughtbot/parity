89be4ad97253f74c7fb66fa84a655e5710bcffb1
Gabe Berke-Williams <gabebw@gabebw.com>
2015-03-18 11:51:26 -0400

    Pass subcommands to shims
    The last line of each shim is (e.g.):
    
    exec "$SELF_DIR/../lib/ruby/bin/ruby" "$SELF_DIR/../lib/app/bin/staging"
    
    Note that this is running `staging` and not passing along any arguments passed
    to the shim, so all subcommands are ignored. Running `staging open` is passed
    along as just `staging`.
    
    The fix is to pass along arguments passed to the shim with `"$@"`:
    
    exec "$SELF_DIR/../lib/ruby/bin/ruby" "$SELF_DIR/../lib/app/bin/staging" "$@"

--- packaging/development_shim.sh
+++ packaging/development_shim.sh
@@ -6,4 +6,4 @@ REALPATH=$(readlink $0 || $0)
 RELATIVE_DIR=$(dirname $REALPATH)
 SELF_DIR=$(cd $(dirname $0) && cd $RELATIVE_DIR && pwd)
 
-exec "$SELF_DIR/../lib/ruby/bin/ruby" "$SELF_DIR/../lib/app/bin/development"
+exec "$SELF_DIR/../lib/ruby/bin/ruby" "$SELF_DIR/../lib/app/bin/development" "$@"
