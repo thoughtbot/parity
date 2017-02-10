ea23f263085de7a9d9995c59d7371b5e5941f6d0
Geoff Harcourt <geoff.harcourt@gmail.com>
2017-02-10 15:22:46 -0500

    Remove Traveling Ruby
    Traveling Ruby is stuck on a version of Bundler that has a bug that
    prevents us from packaging `Gemfile.lock` without needing to touch it on
    the installed machine's filesystem. There hasn't been a commit in almost
    ten months, and it appears that development has stopped.
    
    We're going to try packaging Parity directly and using system Ruby
    instead of bundling our own Ruby executable. This change removes all of
    the packaging tooling related to Traveling Ruby.

--- packaging/staging_shim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-REALPATH=$(readlink $0 || $0)
-RELATIVE_DIR=$(dirname $REALPATH)
-SELF_DIR=$(cd $(dirname $0) && cd $RELATIVE_DIR && pwd)
-
-exec "$SELF_DIR/../lib/ruby/bin/ruby" "$SELF_DIR/../lib/app/bin/staging" "$@"
